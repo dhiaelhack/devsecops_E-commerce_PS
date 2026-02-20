43287fc757a47e7cccf181e2a1e9bc3eb31480a9
nsibidhiaelhack <nsibidhiaelhack@gmail.com>
2026-02-20 05:22:53 +0100

    i add a rootcontreller to pass the test

--- back/src/main/java/com/example/demo/controllers/RootController.java
+++ back/src/main/java/com/example/demo/controllers/RootController.java
@@ -3,10 +3,10 @@ package com.example.demo.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController // This MUST be @RestController to return raw strings
 public class RootController {
 
-    @GetMapping("/") // This corresponds to the "/" in your test
+    @GetMapping("/")
     public String home() {
         return "Spring is here!";
     }
